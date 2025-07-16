f9df87c9835fb4880c87f9e80f2de84115ebec76
Chitra Bahadur Thapa Chhetri <geevanthapa750@gmail.com>
2025-07-16 11:42:32 +0545

    blog, comment and users added

--- Blog App/src/models/Comment.js
+++ Blog App/src/models/Comment.js
@@ -2,6 +2,20 @@ const mongoose = require('mongoose')
 
 const commentSchema = new mongoose.Schema({
 
+    blog:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref:'Blog'
+
+    },
+    user:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:'User'
+
+    },
+    text:{
+        type: String,
+        required: true,
+    }
 })
 
 module.exports = mongoose.model('Comment', commentSchema)
